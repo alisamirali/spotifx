e04bea1539db5ada8022f7b76f986121095dc389
Ali Samir <alisamir.eng@gmail.com>
2023-06-29 14:59:23 +0300

    Edit Scrollbar

--- components/Library.tsx
+++ components/Library.tsx
@@ -16,9 +16,7 @@ interface LibraryProps {
   songs: Song[];
 }
 
-const Library: React.FC<LibraryProps> = ({
-  songs
-}) => {
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const { user, subscription } = useUser();
   const uploadModal = useUploadModal();
   const authModal = useAuthModal();
@@ -36,20 +34,18 @@ const Library: React.FC<LibraryProps> = ({
     }
 
     return uploadModal.onOpen();
-  }
+  };
 
-  return ( 
-    <div className="flex flex-col">
+  return (
+    <div className="flex flex-col library__sidebar">
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="text-neutral-400" size={26} />
-          <p className="text-neutral-400 font-medium text-md">
-            Your Library
-          </p>
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
         </div>
-        <AiOutlinePlus 
-          onClick={onClick} 
-          size={20} 
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
           className="
             text-neutral-400 
             cursor-pointer 
@@ -60,15 +56,15 @@ const Library: React.FC<LibraryProps> = ({
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
-          <MediaItem 
-            onClick={(id: string) => onPlay(id)} 
-            key={item.id} 
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
             data={item}
           />
         ))}
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Library;
